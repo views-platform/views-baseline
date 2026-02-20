b114cadacfd6933822042a9d0950e9194681eb36
Dylan <52908667+smellycloud@users.noreply.github.com>
2026-02-20 14:19:21 +0100

    more baselines

--- views_baseline/model/catalog.py
+++ views_baseline/model/catalog.py
@@ -1,4 +1,13 @@
-from views_baseline.model.baseline import ZeroModel, LocfModel, AverageModel, ConflictologyModel, MirrorModel
+from views_baseline.model.baseline import (
+    ZeroModel,
+    LocfModel,
+    AverageModel,
+    ConflictologyModel,
+    MirrorModel,
+    DriftModel,
+    HistoricalAverageModel,
+    ClimatologyModel,
+)
 
 
 class BaselineModelCatalog:
@@ -16,6 +25,9 @@ class BaselineModelCatalog:
             "AverageModel":self._get_average_model,
             "ConflictologyModel":self._get_conflictology_model,
             "MirrorModel": self._get_mirror_model,
+            "DriftModel": self._get_drift_model,
+            "HistoricalAverageModel": self._get_historical_average_model,
+            "ClimatologyModel": self._get_climatology_model,
         }
 
     def get_model(self, model_name: str):
@@ -61,6 +73,28 @@ class BaselineModelCatalog:
 
     def _get_mirror_model(self):
         return MirrorModel(
+            targets=self.config["targets"],
+            partition_dict=self.partition_dict,
+            loa=self.loa
+        )
+
+    def _get_drift_model(self):
+        return DriftModel(
+            targets=self.config["targets"],
+            months=self.config["months"],
+            partition_dict=self.partition_dict,
+            loa=self.loa
+        )
+
+    def _get_historical_average_model(self):
+        return HistoricalAverageModel(
+            targets=self.config["targets"],
+            partition_dict=self.partition_dict,
+            loa=self.loa
+        )
+
+    def _get_climatology_model(self):
+        return ClimatologyModel(
             targets=self.config["targets"],
             partition_dict=self.partition_dict,
             loa=self.loa
